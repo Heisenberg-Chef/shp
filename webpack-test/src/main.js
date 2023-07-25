6f7ca0147cbe314d442262bd0bc8e2d08adde5f2
Heisenebrg <825958926@qq.com>
2023-07-25 10:49:40 +0800

    clean test

--- webpack-test/src/main.js
+++
@@ -1,8 +0,0 @@
-import css from "./css/index.css"
-import count from "./js/count"
-import sum from "./js/sum"
-import less from "./less/index.less"
-
-console.log(count(2, 1));
-
-console.log(sum(1, 2, 3, 4, 5, 6));
